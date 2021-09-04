15231132087f4eea7c5ac4c48e3a78f4f5998ec1
Mukunda Mhasalkar <mukunda.mhasalkar@ignitesol.com>
2021-09-04 21:48:05 +0530

    Fetching API Key from local env

--- src/components/News.js
+++ src/components/News.js
@@ -34,7 +34,7 @@ export class News extends Component {
     async updateNewsArticles() {
         this.setState({loading: true})
         this.props.setProgress(20);
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b8790e35445e4664a93efa57492d483f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         this.props.setProgress(50);
         let parsedData = await data.json();
@@ -49,7 +49,7 @@ export class News extends Component {
 
     fetchMoreData = async () => {
         this.setState({page: this.state.page + 1})
-        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=b8790e35445e4664a93efa57492d483f&page=${this.state.page}&pageSize=${this.props.pageSize}`;
+        let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=${this.props.apiKey}&page=${this.state.page}&pageSize=${this.props.pageSize}`;
         let data = await fetch(url);
         let parsedData = await data.json();
         // console.log("Articles data:", parsedData.articles)
